f064c168037db6a29753251d5cc79d7fb5a4737a
Nicholas Thomson <nthomsondev@gmail.com>
2011-01-07 22:42:56 -0500

    Change <?= tags to <?php tags with echos to support more systems.

--- include/nav.php
+++ include/nav.php
@@ -5,9 +5,9 @@ $active[$current] = 'class="selected"';
 ?>			
 <nav class="right">
 					<ul class="nav">
-						<li><a href="./"  <?=$active[1]?>>home</a> |</li>
-						<li><a href="portfolio.php"  <?=$active[2]?>>portfolio</a> |</li>
-						<li><a href="contact.php"  <?=$active[3]?>>contact</a> |</li>
+						<li><a href="./"  <?php echo $active[1]?>>home</a> |</li>
+						<li><a href="portfolio.php"  <?php echo $active[2]?>>portfolio</a> |</li>
+						<li><a href="contact.php"  <?php echo $active[3]?>>contact</a> |</li>
 						<li><a href="docs/resume.pdf">resum&eacute;</a></li>
 					</ul>
 				</nav>
