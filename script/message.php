6034ee307ec9e464ede6ba391a7c6abc4dcc89fe
nthomson <nthomsondev@gmail.com>
2011-04-09 00:12:45 -0400

    Minor success reply wording changes

--- script/message.php
+++ script/message.php
@@ -12,7 +12,7 @@ $body = $_POST['message'];
 $headers = "From: $from\r\n"."X-Mailer: php";
 
 	if(mail($to, $subject, $body, $headers)){
-		echo("Message sent successfully - I'll get back to you soon!");
+		echo("Message sent successfully - I'll get back to you as soon as I can!");
 	}
 	else {
 		echo("Error: Message failed to send, please try an alternative method of contact");
